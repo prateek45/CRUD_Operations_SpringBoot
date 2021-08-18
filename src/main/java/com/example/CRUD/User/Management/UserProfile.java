e2253738fc0e3d611b8aa92f9f4de007f1de9d84
Gupta, Prateek <i-prateek.gupta@monsterindia.com>
2021-08-18 17:28:39 +0530

    Enabling Lombok

--- src/main/java/com/example/CRUD/User/Management/UserProfile.java
+++ src/main/java/com/example/CRUD/User/Management/UserProfile.java
@@ -1,14 +1,12 @@
 package com.example.CRUD.User.Management;
 
-public class UserProfile {
+import lombok.Data;
+
+@Data public class UserProfile {
     private String name;
     private String phoneNumber;
     private String address;
     private String email;
-
-    public UserProfile(){
-        // for easy creation of constructor
-    }
 
     public UserProfile(String name, String phoneNumber, String address, String email) {
         this.name = name;
@@ -17,35 +15,4 @@ public class UserProfile {
         this.email = email;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
